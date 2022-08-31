2be531389428dd318e9fa68ae4e7b53f067d935c
DerpyNewbie <derpynewbie@gmail.com>
2022-09-01 03:30:15 +0900

    Remove fancy stuff from PackageUpdater

--- Packages/dev.derpynewbie.common/Editor/NewbieCommonsPackageUpdater.cs
+++ Packages/dev.derpynewbie.common/Editor/NewbieCommonsPackageUpdater.cs
@@ -5,53 +5,35 @@ using UnityEngine;
 
 namespace DerpyNewbie.Common.Editor
 {
-    [InitializeOnLoad]
-    public class PackageUpdater
+    public static class NewbieCommonsPackageUpdater
     {
-        public delegate void PackageUpdateEventHandler();
-
-        public static event PackageUpdateEventHandler UpdateEvent;
-
-        [MenuItem("Newbie Commons/Update Packages")]
+        [MenuItem("Newbie Commons/Update NewbieCommons")]
         public static void UpdatePackages()
-        {
-            UpdateEvent?.Invoke();
-        }
-
-        private static void UpdateNewbieCommonsPackage()
         {
             var request =
                 Client.Add("https://github.com/DerpyNewbie/NewbieCommons.git?path=/Packages/dev.derpynewbie.common");
 
-            var progress = 0F;
             while (!request.IsCompleted)
             {
-                EditorUtility.DisplayProgressBar(
-                    "Updating Package",
-                    "Updating NewbieCommons Package",
-                    progress += 0.1F);
                 Thread.Sleep(100);
+                EditorUtility.DisplayProgressBar("Updating NewbieCommons Package", "Updating NewbieCommons Package", 0);
             }
 
             EditorUtility.ClearProgressBar();
-
             switch (request.Status)
             {
                 case StatusCode.Success:
-                    Debug.Log($"{request.Result.displayName} is now {request.Result.version}");
+                    EditorUtility.DisplayDialog("Updating NewbieCommons Package",
+                        $"Successfully updated NewbieCommons package to {request.Result.version}@{request.Result.git.hash}", "OK!");
                     break;
                 case StatusCode.Failure:
-                    Debug.LogError($"{request.Error.message}");
+                    EditorUtility.DisplayDialog("Updating NewbieCommons Package",
+                        $"Failed to update Package\n{request.Error.message}", "OK!");
                     break;
                 case StatusCode.InProgress:
                     Debug.LogWarning("NewbieCommons Package Update is in progress?");
                     break;
             }
-        }
-
-        static PackageUpdater()
-        {
-            UpdateEvent += UpdateNewbieCommonsPackage;
         }
     }
 }
