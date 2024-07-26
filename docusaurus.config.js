13da19cb423fc9015548197bf22346bb35b7a61e
DerpyNewbie <derpynewbie@gmail.com>
2024-07-27 03:05:38 +0900

    Don't throw when broken links are found

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'DerpyNewbie', // Usually your GitHub org/user name.
   projectName: 'docs-NewbieCommons', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
