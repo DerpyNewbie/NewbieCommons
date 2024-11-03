a1c18b7d90822eda2726c363c83ca10abfc288a1
DerpyNewbie <derpynewbie@gmail.com>
2024-11-03 17:02:56 +0900

    Add collision related invokers

---
+++ Packages/dev.derpynewbie.common/Runtime/Invoker/OnCollisionEnterInvoker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using VRC.SDKBase;
+
+namespace DerpyNewbie.Common.Invoker
+{
+    [AddComponentMenu("Newbie Commons/Invoker/Collision/On Collision Enter")]
+    public class OnCollisionEnterInvoker : CommonInvokerBase
+    {
+        [SerializeField]
+        private bool isTriggeringOnLocalOnly = true;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject == null) return;
+
+            if (isTriggeringOnLocalOnly && !Networking.IsOwner(collision.gameObject))
+                return;
+            Invoke();
+        }
+    }
+}
--- Packages/dev.derpynewbie.common/Runtime/Invoker/OnPlayerTriggerExitInvoker.cs
+++ Packages/dev.derpynewbie.common/Runtime/Invoker/OnPlayerTriggerExitInvoker.cs
@@ -3,13 +3,13 @@ using VRC.SDKBase;
 
 namespace DerpyNewbie.Common.Invoker
 {
-    [AddComponentMenu("Newbie Commons/Invoker/Collision/On Player Trigger Enter Event")]
-    public class OnPlayerTriggerEnterInvoker : CommonInvokerBase
+    [AddComponentMenu("Newbie Commons/Invoker/Collision/On Player Trigger Exit Event")]
+    public class OnPlayerTriggerExitInvoker : CommonInvokerBase
     {
         [SerializeField]
         private bool isTriggeringOnLocalOnly = true;
 
-        public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        public override void OnPlayerTriggerExit(VRCPlayerApi player)
         {
             if (isTriggeringOnLocalOnly && player.isLocal == false)
                 return;
