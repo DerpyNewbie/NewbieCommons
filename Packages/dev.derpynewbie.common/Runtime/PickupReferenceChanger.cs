373c3873cc4cbfd4019271c5117c02513ed05604
DerpyNewbie <contact@derpynewbie.dev>
2024-11-24 23:00:08 +0900

    Add PickupReferenceChanger for manipulating pickup ref each by left and right

---
+++ Packages/dev.derpynewbie.common/Runtime/PickupReferenceChanger.cs
@@ -0,0 +1,30 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+
+namespace DerpyNewbie.Common
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class PickupReferenceChanger : UdonSharpBehaviour
+    {
+        [SerializeField] private VRCPickup pickup;
+        [SerializeField] private Transform pickupReference;
+        [SerializeField] private Transform leftHandReference;
+        [SerializeField] private Transform rightHandReference;
+
+        public override void OnPickup()
+        {
+            _SetPickupReference(
+                pickup.currentHand == VRC_Pickup.PickupHand.Left
+                    ? leftHandReference
+                    : rightHandReference
+            );
+        }
+
+        private void _SetPickupReference(Transform t)
+        {
+            pickupReference.SetPositionAndRotation(t.position, t.rotation);
+        }
+    }
+}
