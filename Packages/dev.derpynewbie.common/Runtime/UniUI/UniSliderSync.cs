713f9078eb3394e97f9a2192ec517e994189d809
DerpyNewbie <contact@derpynewbie.dev>
2025-05-31 18:58:58 +0900

    Add UniUI prefabs

---
+++ Packages/dev.derpynewbie.common/Runtime/UniUI/UniSliderSync.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+
+namespace DerpyNewbie.Common.UniUI
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class UniSliderSync : UniUI
+    {
+        [SerializeField] [NewbieInject(SearchScope.Parents)]
+        private Slider slider;
+
+        [UdonSynced] [FieldChangeCallback(nameof(SyncedValue))]
+        private float _syncedValue;
+
+        private bool _inSyncedChangeContext;
+
+        public float SyncedValue
+        {
+            get => _syncedValue;
+            set
+            {
+                _syncedValue = value;
+                if (_inSyncedChangeContext) return;
+
+                _inSyncedChangeContext = true;
+                slider.value = value;
+                _inSyncedChangeContext = false;
+            }
+        }
+
+        private void Start()
+        {
+            if (!Networking.IsMaster) return;
+            _syncedValue = slider.value;
+            RequestSerialization();
+        }
+
+        public override void OnUniUIUpdate()
+        {
+            if (_inSyncedChangeContext || Mathf.Approximately(slider.value, SyncedValue)) return;
+
+            SyncedValue = slider.value;
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+            RequestSerialization();
+        }
+    }
+}
