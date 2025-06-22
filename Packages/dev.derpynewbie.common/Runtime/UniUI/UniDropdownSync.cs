0d27aea796e25feed2356c3887bf85b37b7f3dc5
DerpyNewbie <contact@derpynewbie.dev>
2025-06-22 21:04:22 +0900

    Fix syncing issues for late-joiner by using OnDeserialization

--- Packages/dev.derpynewbie.common/Runtime/UniUI/UniDropdownSync.cs
+++ Packages/dev.derpynewbie.common/Runtime/UniUI/UniDropdownSync.cs
@@ -12,39 +12,35 @@ namespace DerpyNewbie.Common.UniUI
         [SerializeField] [NewbieInject(SearchScope.Parents)]
         private TMP_Dropdown dropdown;
 
-        [UdonSynced] [FieldChangeCallback(nameof(SyncedValue))]
+        [UdonSynced]
         private int _syncedValue;
 
         private bool _inSyncedChangeContext;
 
-        public int SyncedValue
-        {
-            get => _syncedValue;
-            set
-            {
-                _syncedValue = value;
-                if (_inSyncedChangeContext) return;
-
-                _inSyncedChangeContext = true;
-                dropdown.value = value;
-                dropdown.RefreshShownValue();
-                _inSyncedChangeContext = false;
-            }
-        }
-
         private void Start()
         {
+            // serialize the initial value when instance master has first joined
             if (!Networking.IsMaster) return;
             _syncedValue = dropdown.value;
             RequestSerialization();
         }
 
+        public override void OnDeserialization()
+        {
+            // apply deserialized values to UI
+            _inSyncedChangeContext = true;
+            dropdown.value = _syncedValue;
+            dropdown.RefreshShownValue();
+            _inSyncedChangeContext = false;
+        }
+
         public override void OnUniUIUpdate()
         {
-            if (_inSyncedChangeContext || dropdown.value == SyncedValue) return;
+            // don't resync if the event was called by OnDeserialization change
+            if (_inSyncedChangeContext) return;
 
-            SyncedValue = dropdown.value;
-            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+            _syncedValue = dropdown.value;
+            if (!Networking.IsOwner(gameObject)) Networking.SetOwner(Networking.LocalPlayer, gameObject);
             RequestSerialization();
         }
     }
